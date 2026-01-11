c2b27e7d17a1678a7d3d214634256af5a2dc30cc
Ali0lo <aliaze975@gmail.com>
2026-01-11 09:21:57 +0000

    a

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -19,7 +19,7 @@ def frequency():
     plt.title("Project A")
 
     plt.xlim(0, 100)
-    plt. xticks(np.arange(0, 101, 10))
+    plt.xticks(np. arange(0, 101, 10))
     plt.ylim(0, 30)
 
     plt.show()
