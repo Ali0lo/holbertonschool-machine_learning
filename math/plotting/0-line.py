58823c52d8cf3b94096075dd0d47837b51581fa8
Ali0lo <aliaze975@gmail.com>
2026-01-11 09:03:29 +0000

    a

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python3
+"""Module for plotting a line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def line():
+    """Plots y as a solid red line graph."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
     plt.plot(np.arange(0, 11), y, 'r-')
     plt.xlim(0, 10)
     plt.show()
