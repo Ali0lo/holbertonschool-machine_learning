3fe34e69cc418ef8a60d2b8f3ed12a96bba97068
Ali0lo <aliaze975@gmail.com>
2026-02-01 20:00:04 +0000

    A

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to transpose a 2D matrix"""
+
+
+def matrix_transpose(matrix):
+    """Returns the transpose of a 2D matrix
+    
+    Args:
+        matrix: A 2D matrix (list of lists)
+    
+    Returns:
+        A new matrix that is the transpose of the input matrix
+    """
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
