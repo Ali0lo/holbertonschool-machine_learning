c0dd58ccd0de29dec7f3ad1729266ca8a3f08ac5
Ali0lo <aliaze975@gmail.com>
2026-02-05 12:17:47 +0000

    a

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for numpy operations"""
+
+
+def np_slice(matrix, axes={}):
+    """Slices a matrix along specific axes"""
+    slices = [slice(None)] * matrix.ndim
+    for axis, slice_tuple in axes.items():
+        slices[axis] = slice(*slice_tuple)
+    return matrix[tuple(slices)]
