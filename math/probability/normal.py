8e428b2e24ad86247bd051d6d097556d87c3f0b9
Ali0lo <aliaze975@gmail.com>
2026-02-27 05:50:27 +0000

    A

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -48,3 +48,16 @@ class Normal:
             x-value of z
         """
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given x-value
+        Args:
+            x: the x-value
+        Returns:
+            PDF value for x
+        """
+        return (1 / ((2 * 3.1415926536) ** (1 / 2) * self.stddev)
+                * 2.7182818285 ** ((-(x - self.mean) ** 2) /
+                (2 * self.stddev ** 2))
+                )
