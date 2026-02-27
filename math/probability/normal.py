dd84d16f1dc7f880e0818aa9c31b4267fcafc096
Ali0lo <aliaze975@gmail.com>
2026-02-27 05:48:04 +0000

    A

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -16,17 +16,38 @@ class Normal:
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
-            self.mean = float(mean)
-            self.stddev = float(stddev)
+            self.stddev = stddev
+            self.mean = mean
+        elif not isinstance(data, list):
+            raise TypeError("data must be a list")
+        elif len(data) < 2:
+            raise ValueError("data must contain multiple values")
         else:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            
-            # Calculate mean
             self.mean = sum(data) / len(data)
-            
-            # Calculate standard deviation
-            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
-            self.stddev = variance ** 0.5
+            stddev = 0
+            for i in data:
+                stddev += (self.mean - i) ** 2
+            self.stddev = (stddev / len(data)) ** (1 / 2)
+
+    def z_score(self, x):
+        """Calculate z score"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculate x from z score"""
+        return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculate PDF value for given x"""
+        return (1 / ((2 * 3.1415926536) ** (1 / 2) * self.stddev)
+                * 2.7182818285 ** ((-(x - self.mean) ** 2) /
+                (2 * self.stddev ** 2))
+                )
+
+    def cdf(self, x):
+        """Calculate CDF value for given x"""
+        pi = 3.1415926536
+        x = (x - self.mean) / (self.stddev * 2 ** (1 / 2))
+        erf = (x - x ** 3 / 3 + x ** 5 / 10 - x ** 7 / 42 + x ** 9 / 216)
+        erf = 2 * erf / (pi ** (1 / 2))
+        return (1 / 2) * (1 + erf)
