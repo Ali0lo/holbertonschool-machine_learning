ecd63756b8528e6dedbf94cec7c67dd78456758b
Ali0lo <aliaze975@gmail.com>
2026-03-08 19:08:01 +0000

    A

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-"""Module that defines a DeepNeuralNetwork for binary classification."""
+"""Module that defines a deep neural network for binary classification."""
+
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Deep neural network performing binary classification."""
+    """Defines a deep neural network performing binary classification."""
 
     def __init__(self, nx, layers):
-        """Initialize DeepNeuralNetwork.
+        """
+        Class constructor.
 
         Args:
             nx (int): Number of input features.
-            layers (list): Number of nodes in each layer.
-
-        Raises:
-            TypeError: If nx is not an integer.
-            ValueError: If nx is less than 1.
-            TypeError: If layers is not a list of positive integers.
+            layers (list): List representing the number of nodes per layer.
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -32,9 +29,13 @@ class DeepNeuralNetwork:
         self.weights = {}
 
         for l in range(1, self.L + 1):
-            nodes_prev = nx if l == 1 else layers[l - 2]
-            nodes_curr = layers[l - 1]
-            self.weights["W" + str(l)] = (
-                np.random.randn(nodes_curr, nodes_prev) * np.sqrt(2 / nodes_prev)
-            )
-            self.weights["b" + str(l)] = np.zeros((nodes_curr, 1))
+            if l == 1:
+                self.weights["W" + str(l)] = (
+                    np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
+                )
+            else:
+                self.weights["W" + str(l)] = (
+                    np.random.randn(layers[l - 1], layers[l - 2])
+                    * np.sqrt(2 / layers[l - 2])
+                )
+            self.weights["b" + str(l)] = np.zeros((layers[l - 1], 1))
