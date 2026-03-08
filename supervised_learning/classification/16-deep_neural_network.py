9a49ceb1aded5ab2795e2a2b085debe5da50513d
Ali0lo <aliaze975@gmail.com>
2026-03-08 19:02:47 +0000

    A

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-"""Deep Neural Network for binary classification"""
+"""Module that defines a DeepNeuralNetwork for binary classification."""
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network performing binary classification"""
+    """Deep neural network performing binary classification."""
 
     def __init__(self, nx, layers):
+        """Initialize DeepNeuralNetwork.
+
+        Args:
+            nx (int): Number of input features.
+            layers (list): Number of nodes in each layer.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+            TypeError: If layers is not a list of positive integers.
+        """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -15,9 +26,11 @@ class DeepNeuralNetwork:
             raise TypeError("layers must be a list of positive integers")
         if not all(isinstance(n, int) and n > 0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
+
         for l in range(1, self.L + 1):
             nodes_prev = nx if l == 1 else layers[l - 2]
             nodes_curr = layers[l - 1]
