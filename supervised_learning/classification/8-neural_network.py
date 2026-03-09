e732d340b64d645183d3d88015b0e37592c48053
Eldeniz <eldenizarifzade02@gmail.com>
2026-03-09 07:48:07 +0000

    neural net

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" This module will define a class named Neuron """
+import numpy as np
+
+
+class NueralNetwork():
+    """ This class will define a neural network with one hidden layer """
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.randn(1, nx)
+        self.__b1= 0
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nx)
+        self.__b2= 0
+        self.__A2 = 0
