7003d7922a44db0ad010b13179f2bd43f9eec2a6
Eldeniz <eldenizarifzade02@gmail.com>
2026-03-09 07:50:35 +0000

    gitpusher

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -15,9 +15,9 @@ class NeuralNetwork():
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
 
-        self.__W1 = np.random.randn(1, nx)
-        self.__b1= 0
-        self.__A1 = 0
-        self.__W2 = np.random.randn(1, nx)
-        self.__b2= 0
-        self.__A2 = 0
+        self.W1 = np.random.randn(1, nx)
+        self.b1= 0
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nx)
+        self.b2= 0
+        self.A2 = 0
