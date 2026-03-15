536a7d4e040bbabdb203e1f97576fe31d944e83b
Ali0lo <aliaze975@gmail.com>
2026-03-15 14:00:51 +0000

    A

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Trains a Keras model with validation, early stopping, LR decay, and save best."""
+"""Trains a Keras model with callbacks."""
 
 import tensorflow.keras as K
 
@@ -11,22 +11,22 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
     """Trains a model using mini-batch gradient descent."""
     callbacks = []
 
-    if early_stopping is True and validation_data is not None:
+    if early_stopping and validation_data is not None:
         callbacks.append(K.callbacks.EarlyStopping(
             monitor='val_loss',
             patience=patience
         ))
 
-    if learning_rate_decay is True and validation_data is not None:
+    if learning_rate_decay and validation_data is not None:
         def schedule(epoch):
             return alpha / (1 + decay_rate * epoch)
 
         callbacks.append(K.callbacks.LearningRateScheduler(
-            schedule=schedule,
+            schedule,
             verbose=1
         ))
 
-    if save_best is True and validation_data is not None and filepath is not None:
+    if save_best and validation_data is not None and filepath is not None:
         callbacks.append(K.callbacks.ModelCheckpoint(
             filepath=filepath,
             monitor='val_loss',
@@ -38,8 +38,8 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
         y=labels,
         batch_size=batch_size,
         epochs=epochs,
+        validation_data=validation_data,
         verbose=verbose,
         shuffle=shuffle,
-        validation_data=validation_data,
         callbacks=callbacks
     )
