a593d8969c2fd2a94a155797ca09d5a1af46ceca
Ali0lo <aliaze975@gmail.com>
2026-03-15 14:00:25 +0000

    A

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Makes predictions using a Keras neural network."""
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """Makes a prediction using a neural network."""
+    return network.predict(
+        x=data,
+        verbose=verbose
+    )
