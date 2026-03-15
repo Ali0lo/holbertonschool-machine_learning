311dfc98b5bb770fef81f3bf7ecf0d55e9130dea
Ali0lo <aliaze975@gmail.com>
2026-03-15 16:12:01 +0000

    A

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Calculates specificity for each class in a confusion matrix."""
+
+import numpy as np
+
+
+def specificity(confusion):
+    """Returns the specificity of each class."""
+    classes = confusion.shape[0]
+    total = np.sum(confusion)
+
+    true_positives = np.diag(confusion)
+    false_positives = np.sum(confusion, axis=0) - true_positives
+    false_negatives = np.sum(confusion, axis=1) - true_positives
+    true_negatives = total - (true_positives + false_positives + false_negatives)
+
+    return true_negatives / (true_negatives + false_positives)
