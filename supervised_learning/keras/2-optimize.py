f9841409aac047188acd89888de1c9697c004920
Ali0lo <aliaze975@gmail.com>
2026-03-15 13:15:49 +0000

    A

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Sets up Adam optimization for a Keras model."""
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Sets up Adam optimization for a keras model.
+
+    Args:
+        network: The model to optimize.
+        alpha (float): Learning rate.
+        beta1 (float): First Adam optimization parameter.
+        beta2 (float): Second Adam optimization parameter.
+
+    Returns:
+        None
+    """
+    optimizer = K.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2
+    )
+    network.compile(
+        optimizer=optimizer,
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
