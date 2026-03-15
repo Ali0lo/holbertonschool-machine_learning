2549a32e0fa0635ac2325b69d949f36cac5e51c9
Ali0lo <aliaze975@gmail.com>
2026-03-15 14:01:23 +0000

    A

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
-"""Trains Keras model with validation and early stopping."""
+"""Trains Keras model with optional validation and early stopping."""
 
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None,
-                early_stopping=False, patience=0, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent."""
     callbacks = []
 
-    if early_stopping is True and validation_data is not None:
-        early_stop = K.callbacks.EarlyStopping(
+    if early_stopping and validation_data is not None:
+        callbacks.append(K.callbacks.EarlyStopping(
             monitor='val_loss',
             patience=patience
-        )
-        callbacks.append(early_stop)
+        ))
 
     return network.fit(
         x=data,
         y=labels,
         batch_size=batch_size,
         epochs=epochs,
+        validation_data=validation_data,
         verbose=verbose,
         shuffle=shuffle,
-        validation_data=validation_data,
         callbacks=callbacks
     )
