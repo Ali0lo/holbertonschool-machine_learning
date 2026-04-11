7d7e5bebc1e53f1d791922417c56fd4cc680efc8
Ali0lo <aliaze975@gmail.com>
2026-04-12 00:57:08 +0400

    Implement PCA color augmentation

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
-"""PCA Color Augmentation as described in the AlexNet paper."""
+"""PCA Color Augmentation"""
 import tensorflow as tf
 
 
 def pca_color(image, alphas):
-    """Performs PCA color augmentation on an image.
-
-    Args:
-        image: 3D tf.Tensor containing the image to change
-        alphas: tuple of length 3 containing the amount each
-                principal component should change
-
-    Returns:
-        The augmented image
-    """
+    """Performs PCA color augmentation"""
     image = tf.cast(image, tf.float32)
 
     pixels = tf.reshape(image, (-1, 3))
@@ -25,7 +16,12 @@ def pca_color(image, alphas):
 
     eigenvalues, eigenvectors = tf.linalg.eigh(cov)
 
+    # 🔥 FIX: reverse order (largest first)
+    eigenvalues = eigenvalues[::-1]
+    eigenvectors = eigenvectors[:, ::-1]
+
     alphas = tf.cast(alphas, tf.float32)
+
     delta = tf.matmul(
         eigenvectors,
         tf.reshape(alphas * eigenvalues, (3, 1))
