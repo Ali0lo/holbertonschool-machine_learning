eda88960119a1ccd39b17876360ee87065596ff1
Ali0lo <aliaze975@gmail.com>
2026-04-12 01:03:51 +0400

    Implement PCA color augmentation

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -4,31 +4,28 @@ import tensorflow as tf
 
 
 def pca_color(image, alphas):
-    """Performs PCA color augmentation"""
+    """Performs PCA color augmentation on an image"""
     image = tf.cast(image, tf.float32)
 
     pixels = tf.reshape(image, (-1, 3))
     mean = tf.reduce_mean(pixels, axis=0)
     centered = pixels - mean
 
-    n = tf.cast(tf.shape(centered)[0], tf.float32)
-    cov = tf.matmul(centered, centered, transpose_a=True) / (n - 1)
+    cov = tf.matmul(centered, centered, transpose_a=True)
+    cov /= tf.cast(tf.shape(centered)[0] - 1, tf.float32)
 
     eigenvalues, eigenvectors = tf.linalg.eigh(cov)
 
-    # 🔥 FIX: reverse order (largest first)
-    eigenvalues = eigenvalues[::-1]
-    eigenvectors = eigenvectors[:, ::-1]
+    eigenvalues = tf.reverse(eigenvalues, axis=[0])
+    eigenvectors = tf.reverse(eigenvectors, axis=[1])
 
     alphas = tf.cast(alphas, tf.float32)
-
     delta = tf.matmul(
         eigenvectors,
         tf.reshape(alphas * eigenvalues, (3, 1))
     )
-    delta = tf.reshape(delta, (1, 1, 3))
 
-    augmented = image + delta
-    augmented = tf.clip_by_value(augmented, 0, 255)
+    image = image + tf.reshape(delta, (1, 1, 3))
+    image = tf.clip_by_value(image, 0, 255)
 
-    return tf.cast(augmented, tf.uint8)
+    return tf.cast(image, tf.uint8)
