5a355ed4b00032fbccc923fb8a2462ec7f46d3b8
Ali0lo <aliaze975@gmail.com>
2026-05-24 18:23:48 +0000

    A

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
+"""K-means clustering"""
 import numpy as np
-initialize = __import__('0-initialize').initialize
+
+
+def initialize(X, k):
+    """initializes cluster centroids"""
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    mins = np.min(X, axis=0)
+    maxs = np.max(X, axis=0)
+
+    return np.random.uniform(mins, maxs, (k, X.shape[1]))
 
 
 def kmeans(X, k, iterations=1000):
+    """performs K-means clustering"""
+
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
 
@@ -15,9 +32,15 @@ def kmeans(X, k, iterations=1000):
 
     n, d = X.shape
 
+    if k > n:
+        return None, None
+
     C = initialize(X, k)
 
-    for _ in range(iterations):
+    if C is None:
+        return None, None
+
+    for i in range(iterations):
 
         distances = np.linalg.norm(
             X[:, np.newaxis] - C,
@@ -29,19 +52,32 @@ def kmeans(X, k, iterations=1000):
         new_C = np.copy(C)
 
         for j in range(k):
+
             points = X[clss == j]
 
-            if len(points) == 0:
+            if points.shape[0] == 0:
+
                 new_C[j] = np.random.uniform(
                     np.min(X, axis=0),
-                    np.max(X, axis=0)
+                    np.max(X, axis=0),
+                    (1, d)
                 )
+
             else:
+
                 new_C[j] = np.mean(points, axis=0)
 
         if np.allclose(C, new_C):
-            return new_C, clss
 
-        C = new_C
+            return C, clss
+
+        C = np.copy(new_C)
+
+    distances = np.linalg.norm(
+        X[:, np.newaxis] - C,
+        axis=2
+    )
+
+    clss = np.argmin(distances, axis=1)
 
     return C, clss
