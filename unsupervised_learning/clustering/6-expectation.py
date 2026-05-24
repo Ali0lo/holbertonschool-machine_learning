56f616f14590c466d88a27da3fa6380b1ae0f25d
Ali0lo <aliaze975@gmail.com>
2026-05-24 18:27:23 +0000

    A

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,19 +1,47 @@
 #!/usr/bin/env python3
+"""Expectation step for GMM"""
 import numpy as np
+
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
+    """calculates expectation step"""
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
 
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+
+    n, d = X.shape
     k = pi.shape[0]
 
-    probs = np.array([
-        pi[i] * pdf(X, m[i], S[i])
-        for i in range(k)
-    ])
+    if m.shape != (k, d):
+        return None, None
+
+    if S.shape != (k, d, d):
+        return None, None
+
+    if not np.isclose(np.sum(pi), 1):
+        return None, None
+
+    probs = np.zeros((k, n))
+
+    for i in range(k):
+
+        P = pdf(X, m[i], S[i])
+
+        if P is None:
+            return None, None
+
+        probs[i] = pi[i] * P
 
     total = np.sum(probs, axis=0)
 
